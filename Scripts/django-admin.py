84a2bb38797eadc90a2d14ca821f1677b3e1f775
qwnize@outlook.com <qwnize@outlook.com>
2021-02-10 23:40:01 +0300

    e-commerce before changes

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\basinke\desktop\ecommecre django guide\django_project_boilerplate-master\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
